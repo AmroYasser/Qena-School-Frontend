35c00cc7dc258c756b5bf591410b3ba3df9582a5
mohamed <main.20111998@gmail.com>
2021-04-16 15:10:39 +0200

    update show teacher component

--- src/app/components/teacher/show-teacher/show-teacher.component.ts
+++ src/app/components/teacher/show-teacher/show-teacher.component.ts
@@ -1,3 +1,4 @@
+import { ICourse } from './../../../../models/interfaces/icourse';
 import { Iteacher } from './../../../../models/interfaces/iteacher';
 import { Component, OnInit } from '@angular/core';
 import { TeachersService } from 'src/services/teachers.service';
@@ -10,15 +11,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ShowTeacherComponent implements OnInit {
   teacher: Iteacher | undefined;
-  id:number
+  id: number
+  teacher_groups: ICourse[] = [];
   constructor(private _apiTeacherService: TeachersService, private activedRoute: ActivatedRoute) {
-    this.id=NaN
-   }
+    this.id = NaN
+  }
 
   ngOnInit(): void {
-    this.id= this.activedRoute.snapshot.params["id"];
+    this.id = this.activedRoute.snapshot.params["id"];
     this._apiTeacherService.getSpecificTeacher(this.id).subscribe((res) => {
       this.teacher = res;
+    }, (err) => {
+      console.log(err);
+    });
+    this._apiTeacherService.get_groups_for_teacher(this.id).subscribe((res) => {
+      this.teacher_groups = res;
     }, (err) => {
       console.log(err);
     });
