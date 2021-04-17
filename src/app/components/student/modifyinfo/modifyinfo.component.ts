9eabd19c1173d44fd8191cfa3ff8caad302448d9
Mohamed Ahmed Hgagy <hgagy.hmada@gmail.com>
2021-04-17 16:48:35 +0200

    alter update student component

--- src/app/components/student/modifyinfo/modifyinfo.component.ts
+++ src/app/components/student/modifyinfo/modifyinfo.component.ts
@@ -1,7 +1,7 @@
 import { formatDate } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IStudent } from 'src/models/interfaces/istudent';
 import { StudentsService } from 'src/services/students.service';
 
@@ -15,7 +15,7 @@ student:IStudent
 id :number
 levels:string[]
 image:File |undefined|any
-  constructor(private _studentserv:StudentsService,private _route:ActivatedRoute,private _http:HttpClient) { 
+  constructor(private _studentserv:StudentsService,private _route:ActivatedRoute,private _http:HttpClient,private _router:Router) { 
     this.student={name:"",level:"",phone:""}
     this.id=0
     this.levels = ['اول ابتدائي', 'ثاني ابتدائي', 'ثالث ابتدائي', 'رابع ابتدائي', 'خامس ابتدائي', 'سادس ابتدائي', 'اول اعدادي', 'ثاني اعدادي', 'ثالث اعدادي', 'اول ثانوي', 'ثاني ثانوي', 'ثالث ثانوي', 'اخري']
@@ -30,13 +30,20 @@ image:File |undefined|any
     this.image=event.target.files[0];
     
   }
+
+  reload() {
+    this._router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this._router.onSameUrlNavigation = 'reload';
+    this._router.navigate(['./'], { relativeTo: this._route });
+  }
+
   save(){
     let formData=new FormData()
     formData.append('name',this.student.name)
     formData.append('level',this.student.level)
     formData.append('phone',this.student.phone)
     formData.append('image',this.image)
-    this._http.put(`http://127.0.0.1:8000/student/${this.id}/`,formData).subscribe(res=>console.log(res),
+    this._http.put(`http://127.0.0.1:8000/student/${this.id}/`,formData).subscribe(res=>this.reload(),
     err=>console.log(err))
   }
 
