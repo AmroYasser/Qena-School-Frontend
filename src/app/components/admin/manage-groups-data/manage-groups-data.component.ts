4131fc73fef1d62dcc37e9a847db7cf4406682f1
Mohamed Ahmed Hgagy <hgagy.hmada@gmail.com>
2021-04-17 02:19:38 +0200

    delete teacher component and linking group and teacher urls

--- src/app/components/admin/manage-groups-data/manage-groups-data.component.ts
+++ src/app/components/admin/manage-groups-data/manage-groups-data.component.ts
@@ -1,6 +1,8 @@
 import { CoursesService } from './../../../../services/courses.service';
 import { ICourse } from './../../../../models/interfaces/icourse';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage-groups-data',
@@ -10,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 export class ManageGroupsDataComponent implements OnInit {
   groups: ICourse[] = [];
 
-  constructor(private _apiGroupService: CoursesService) { }
+  constructor(private _apiGroupService: CoursesService,private _http:HttpClient,private _router:Router,private _activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
     this._apiGroupService.getAllCourses().subscribe((res) => {
@@ -21,5 +23,18 @@ export class ManageGroupsDataComponent implements OnInit {
       }
     )
   }
+
+  reload() {
+    this._router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this._router.onSameUrlNavigation = 'reload';
+    this._router.navigate(['./'], { relativeTo: this._activatedRoute });
+  }
+
+  delete_group(id: any){
+    this._http.delete(`http://127.0.0.1:8000/course-group/${id}/`).subscribe(res=>this.reload(),
+    err=>console.log(err))
+
+  }
+ 
 
 }
