c65461afc87908405dd832d7181d7a418677e637
mohamed <main.20111998@gmail.com>
2021-04-23 16:18:10 +0200

    modify add student component and add search in courses component

--- src/app/components/Authentication/login/login.component.ts
+++ src/app/components/Authentication/login/login.component.ts
@@ -11,39 +11,40 @@ import { LoginserviceService } from 'src/services/loginservice.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- email:string=''
- password:string=''
- message:string=''
- user :IUser
-//  logged_in:boolean|any=false
-  constructor(private http:HttpClient, private router:Router,private loginserv:LoginserviceService) {
-    this.user={id:1}
-   }
+  email: string = ''
+  password: string = ''
+  message: string = ''
+  user: IUser
+  //  logged_in:boolean|any=false
+  constructor(private http: HttpClient, private router: Router, private loginserv: LoginserviceService) {
+    this.user = { id: 1 }
+  }
 
   ngOnInit(): void {
   }
 
-  login():void{
-   const formData=new FormData()
-   formData.append('email',this.email)
-   formData.append('password',this.password)
+  login(): void {
+    const formData = new FormData()
+    formData.append('email', this.email)
+    formData.append('password', this.password)
 
-   this.http.post('http://127.0.0.1:8000/auth/login',formData,{withCredentials:true}).subscribe((res)=>{
-     console.log(res);
-     
+    this.http.post('http://127.0.0.1:8000/auth/login', formData, { withCredentials: true }).subscribe((res) => {
+      console.log(res);
+      // document.cookie = res.toString();
 
-    this.http.get('http://127.0.0.1:8000/auth/user',{withCredentials:true}).subscribe((res)=>{console.log(res,"check")
-  },
-    err=>console.log(err));
-    ;
-    
-    this.router.navigate(['/home'])},
-   (err)=>{
-     this.message=err.error.detail;
-     ;
-    console.log(this.message)
-    }
-         )
+      this.http.get('http://127.0.0.1:8000/auth/user', { withCredentials: true }).subscribe((res) => {
+        console.log(res, "check")
+      },
+        err => console.log(err));
+      ;
+
+      this.router.navigate(['/home'])
+    },
+      (err) => {
+        this.message = err.error.detail;
+        console.log(this.message)
+      }
+    )
 
   }
 
